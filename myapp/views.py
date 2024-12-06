c34653d7bd2cb15f1ec899655183914650d5e6b4
Alejandro Rivera <jalejandro142@gmail.com>
2024-12-06 14:17:28 -0500

    Clase sobre formulario metodo get y post

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Project, Task
+from .forms import CreateNewTask
 # Create your views here.
 
 
@@ -37,3 +38,14 @@ def task(request):
     return render(request, 'tasks.html', {
         'tasks': tasks
     })
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('/tasks/')
